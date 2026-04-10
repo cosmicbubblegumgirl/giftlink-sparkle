a4ccb8cdbce976890e71781b7063a36364ab8adc
cosmiccupcake <simon3m3ll3m@gmail.com>
2026-04-10 23:13:47 +0200

    Update db.js

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,3 +1,29 @@
+// giftlink-backend/db.js
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB Atlas!");
+    return client;
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
 /* jshint esversion: 11, node: true */
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const logger = require('../logger');
